7815c7fbc2027c7478c57e4cef43fce3034973cb
divyatadimeti <46757986+divyatadimeti@users.noreply.github.com>
2021-06-29 13:38:30 -0700

    Create Trees-Ex2.java

---
+++ Assignment 2/Trees-Ex2.java
@@ -0,0 +1,19 @@
+class Solution {
+
+  public String printLevelByLevel(Employee ceo) {
+     Queue<Employee> q = new LinkedList<>();
+     q.add(ceo);
+
+     while (q.size() > 0) {
+       for (int i = 0; i < q.size(); i++) {
+         Employee curr_employee = q.poll();
+         System.out.printf("Name: %s, Title: %s\n", curr_employee.name, curr_employee.title);
+         if (!curr_employee.directReports.isEmpty()) {
+           for (int k = 0; k < curr_employee.directReports.size(); k++) {
+             q.add(curr_employee.directReports.get(k));
+           }
+         }
+       }
+     }
+  }
+}
