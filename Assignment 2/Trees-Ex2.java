2489d36679a97d03fd6e10a97c5ccb076b9b9ab8
divyatadimeti <46757986+divyatadimeti@users.noreply.github.com>
2021-06-29 14:42:42 -0700

    Update Trees-Ex2.java
    removed for loop

--- Assignment 2/Trees-Ex2.java
+++ Assignment 2/Trees-Ex2.java
@@ -4,14 +4,12 @@ class Solution {
      Queue<Employee> q = new LinkedList<>();
      q.add(ceo);
 
-     while (q.size() > 0) {
-       for (int i = 0; i < q.size(); i++) {
-         Employee curr_employee = q.poll();
-         System.out.printf("Name: %s, Title: %s\n", curr_employee.name, curr_employee.title);
-         if (!curr_employee.directReports.isEmpty()) {
-           for (int k = 0; k < curr_employee.directReports.size(); k++) {
-             q.add(curr_employee.directReports.get(k));
-           }
+     while (q.size() != 0) {
+       Employee curr_employee = q.poll();
+       System.out.printf("Name: %s, Title: %s\n", curr_employee.name, curr_employee.title);
+       if (!curr_employee.directReports.isEmpty()) {
+         for (int k = 0; k < curr_employee.directReports.size(); k++) {
+           q.add(curr_employee.directReports.get(k));
          }
        }
      }
